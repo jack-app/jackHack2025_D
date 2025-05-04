c5977a9d8f1f59d9e72468abd54620b52b3f62bc
kotama7 <kotamatakanori2@gmail.com>
2025-05-04 03:08:18 +0000

    add:React architecture add

---
+++ love_simulation/src/pages/credit/credit.jsx
@@ -0,0 +1,12 @@
+const Credit = () => {
+    return (
+        <div className="credit">
+        <h1>Credit</h1>
+        <p>Developed by: Your Name</p>
+        <p>Special thanks to: Your Team</p>
+        <p>Version: 1.0.0</p>
+        </div>
+    );
+    }
+
+export default Credit;
---
+++ love_simulation/src/pages/scenario_select/scenario_select.jsx
@@ -0,0 +1,10 @@
+const ScenarioSelect = () => {
+    return (
+        <div>
+        <h1>Scenario Select</h1>
+        <p>Select a scenario to play.</p>
+        </div>
+    );
+    }
+
+export default ScenarioSelect;
