42da7b21f2386e80ca3fe1ae2bcf1b5cf9c3c80e
kotama7 <kotamatakanori2@gmail.com>
2025-05-04 17:30:09 +0900

    add:html convert into jsx

--- love_simulation/src/pages/home/components/start_button.jsx
+++ love_simulation/src/pages/home/components/start_button.jsx
@@ -8,29 +8,9 @@ const StartButton = () => {
       navigate('/scenario_select', { replace: true });
     };
 
-  const styles = {
-    body: {
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: '100vh', // viewport height for vertical centering
-      width: '100%',
-      margin: 0,
-    },
-    button: {
-      backgroundColor: 'rgb(236, 141, 222)',
-      height: '10%',
-      border: '6px solid #E8B3E5',
-      borderRadius: '70%',
-      width: '9%',
-      fontFamily: `'Times New Roman', Times, serif`,
-      fontSize: 'large',
-      color: 'rgb(255, 255, 255)',
-    },
-  };
   return (
-    <div style={styles.body}>
-      <button style={styles.button} onClick={handleClick}>tap to start</button>
+    <div className="startButton">
+      <button onClick={handleClick} id="startButton">tap to start</button>
     </div>
   );
 };
