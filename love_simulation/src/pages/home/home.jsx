f0034963958e7b817aa17714f2f652f813f19a8f
nose <hikasuke20050627@gmail.com>
2025-05-06 12:43:18 +0900

    タイトル画面、シナリオセレクト画面のデザインを変更

--- love_simulation/src/pages/home/home.jsx
+++ love_simulation/src/pages/home/home.jsx
@@ -1,16 +1,32 @@
+import { useState, useEffect } from "react";
 import StartButton from "./components/start_button";
 import CreditButton from "./components/credit_button";
 import "./home.css";
 
 const Home = () => {
+    const backgrounds = [
+        '/images/school_bench_noon.jpg',
+        '/images/school_bench_night.jpg',
+        '/images/restaurant_evening.jpg'
+    ];
+    const [currentBg, setCurrentBg] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentBg((prev) => (prev + 1) % backgrounds.length);
+        }, 8000); // 8秒ごとに切り替え
+
+        return () => clearInterval(interval);
+    }, []);
+
     return (
-        <div className="home">
+        <div className="home" style={{ backgroundImage: `url(${backgrounds[currentBg]})` }}>
             <p id="title1">恋愛cancelシミュレーション</p>
             <p id="title2">恋Can</p>
             <StartButton />
             <CreditButton />
         </div>
     );
-    }
+}
 
 export default Home;
